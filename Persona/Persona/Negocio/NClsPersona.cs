c7cdbc4a32359a5240be5adf630d334b6693be7e
Frank-Rj <87914653+Frank-Rj@users.noreply.github.com>
2021-08-16 22:14:28 -0600

    Mensajes

--- Persona/Persona/Negocio/NClsPersona.cs
+++ Persona/Persona/Negocio/NClsPersona.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Persona.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,27 @@ namespace Persona.Negocio
 {
     class NClsPersona
     {
-    }
-}
+        //Para el segundo ejercicio se va a describir a una “persona”, el repositorio se llamara 
+        //“PrimerLaboratorioPersona”, el proyecto se llamara, Persona, las propiedades serán : tiene nombre, apellido, edad,
+        //sexo, las acciones que las personas pueden hacer serán, hablar, caminar, reír, observar, deveras crear todas, pero
+        //solamente utilizaras una la cual será, hablar, la cual tendrá un mensaje el cual será, “Las persona “Colocar acá el
+        //nombre de la persona” está hablando”, llamar el método Persona el Main, y llamar sus propiedades, de las cual
+        //solamente utilizaras la propiedad de nombre y al nombre le asignaras “Martin” e imprime el mensaje en pantalla que
+        //trae el método Hablar.
+        public String Hablar(ClsPersona persona)
+        {
+            return "La persona " + persona.Nombre + " esta hablando";
+        }
+        public String Caminar(ClsPersona persona)
+        {
+            return persona.Nombre+" de "+ persona.Edad+" camina todos los dias";
+        }
+        public String Reir (ClsPersona persona)
+        {
+            return persona.Nombre + persona.Apellido + " se esta riendo";
+        }
+        public String Observar(ClsPersona persona)
+        {
+            return persona.Nombre + " de sexo " + persona.Sexo + " es observativo";
+        }
+}   }
